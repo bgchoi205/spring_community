6e7f4c70ea9a69c41fdf8bdeb84e64a1a7987a03
bgchoi205 <choibk4938@gmail.com>
2021-08-13 01:11:36 +0900

    jpa,mysql 의존성추가, Member Entity 매핑

--- src/main/java/com/cbg/exam/domain/Member.java
+++ src/main/java/com/cbg/exam/domain/Member.java
@@ -1,7 +1,24 @@
 package com.cbg.exam.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="member")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 200, nullable = false)
     private String name;
 
     public Long getId() {
