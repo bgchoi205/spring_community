6d0b9c7ed2a2bbb21f08e3cfae7854b313d1c6dc
bgchoi205 <choibk4938@gmail.com>
2021-08-12 10:23:49 +0900

    회원등록, 회원목록 예제

--- src/main/java/com/cbg/exam/controller/MemberController.java
+++ src/main/java/com/cbg/exam/controller/MemberController.java
@@ -1,8 +1,15 @@
 package com.cbg.exam.controller;
 
+import com.cbg.exam.domain.Member;
 import com.cbg.exam.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -12,5 +19,26 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/join")
+    public String memberJoin(){
+        return "/members/join";
+    }
+
+    @PostMapping("/members/join")
+    public String memberDoJoin(@RequestParam("name") String name){
+        Member member =  new Member();
+        member.setName(name);
+
+        memberService.save(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String memberList(Model model){
+        List members = memberService.findAll();
+        model.addAttribute("members", members);
+        return "/members/memberList";
     }
 }
