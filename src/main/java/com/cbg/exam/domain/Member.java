fff63e50dbac1c9063ff569de746c0c8cac3aea6
bgchoi205 <choibk4938@gmail.com>
2021-08-13 21:45:43 +0900

    JPA CRUD 테스트

--- src/main/java/com/cbg/exam/domain/Member.java
+++ src/main/java/com/cbg/exam/domain/Member.java
@@ -1,14 +1,12 @@
 package com.cbg.exam.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name="member")
+@ToString
 @Getter
 @Builder
 @AllArgsConstructor
--- src/test/java/com/cbg/exam/repository/MemberRepositoryTest.java
+++ src/test/java/com/cbg/exam/repository/MemberRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @SpringBootTest
 public class MemberRepositoryTest {
@@ -22,6 +23,34 @@ public class MemberRepositoryTest {
         Member member2  = new Member();
         member2.setName("spring2");
         memberRepository.save(member2);
+    }
+
+    @Test
+    public void findMemberById(){
+        Long id = 2L;
+
+        Optional<Member> findedMember = memberRepository.findById(id);
+
+        if(findedMember.isPresent()){
+            Member member = findedMember.get();
+            System.out.println(member);
+        }
+    }
+
+    @Test
+    public void updateMember(){
+        Member member = Member.builder()
+                .id(3L)
+                .name("Hello")
+                .build();
+        memberRepository.save(member);
+    }
+
+    @Test
+    public void deleteMember(){
+        Long id = 4L;
+
+        memberRepository.deleteById(id);
     }
 
 }
