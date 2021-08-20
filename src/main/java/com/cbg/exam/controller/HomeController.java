f73c9c6061dcd8deba739cfcf3a452f291cc8043
bgchoi205 <choibk4938@gmail.com>
2021-08-20 10:37:35 +0900

    테스트데이터생성 메서드를 HomeController로 옮김

--- src/main/java/com/cbg/exam/controller/HomeController.java
+++ src/main/java/com/cbg/exam/controller/HomeController.java
@@ -1,13 +1,46 @@
 package com.cbg.exam.controller;
 
+import com.cbg.exam.domain.Article;
+import com.cbg.exam.domain.Member;
+import com.cbg.exam.service.ArticleService;
+import com.cbg.exam.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final MemberService memberService;
+    private final ArticleService articleService;
 
     @GetMapping("/")
     public String home(){
         return "home";
+    }
+
+    @GetMapping("/home/maketest")
+    public String makeTest(){
+        Member member =  Member.builder()
+                .loginId("aa")
+                .loginPw("aa")
+                .name("aa")
+                .nickname("aa")
+                .email("aa@gmail.com")
+                .build();
+        memberService.save(member);
+
+        for(int i = 1; i <= 10; i++){
+            Article article =  Article.builder()
+                    .memberId(1L)
+                    .boardId(1L)
+                    .title("제목 : " + i)
+                    .body("내용 : " + i)
+                    .build();
+            articleService.save(article);
+        }
+
+        return "redirect:/";
     }
 }
--- src/main/java/com/cbg/exam/controller/MemberController.java
+++ src/main/java/com/cbg/exam/controller/MemberController.java
@@ -21,7 +21,6 @@ import java.util.Optional;
 public class MemberController {
 
     private final MemberService memberService;
-    private final ArticleService articleService;
 
     @GetMapping("/member/login")
     public String memberLogin(){
@@ -82,29 +81,7 @@ public class MemberController {
         return "/member/mypage";
     }
 
-    @GetMapping("/member/maketest")
-    public String makeTest(){
-        Member member =  Member.builder()
-                .loginId("aa")
-                .loginPw("aa")
-                .name("aa")
-                .nickname("aa")
-                .email("aa@gmail.com")
-                .build();
-        memberService.save(member);
 
-        for(int i = 1; i <= 10; i++){
-            Article article =  Article.builder()
-                    .memberId(1L)
-                    .boardId(1L)
-                    .title("제목 : " + i)
-                    .body("내용 : " + i)
-                    .build();
-            articleService.save(article);
-        }
-
-        return "redirect:/";
-    }
 
 //    @GetMapping("/members")
 //    public String memberList(Model model){
