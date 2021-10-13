532c4ddff9f3b096244155e38f5336a31ae82e76
bgchoi205 <choibk4938@gmail.com>
2021-10-13 22:32:46 +0900

    ArticleController RequestMaping 추가, url 방식 변경

--- src/main/java/com/cbg/exam/controller/ArticleController.java
+++ src/main/java/com/cbg/exam/controller/ArticleController.java
@@ -5,25 +5,24 @@ import com.cbg.exam.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/articles")
 public class ArticleController {
 
     private final ArticleService articleService;
 
-    @GetMapping("/usr/article/write")
+    @GetMapping("/write")
     public String articleWrite(){
         return "/usr/article/write";
     }
 
 
-    @PostMapping("/usr/article/write")
+    @PostMapping("/write")
     public String articleDoWrite(
             @RequestParam("title") String title,
             @RequestParam("body") String body
@@ -50,7 +49,7 @@ public class ArticleController {
         return "redirect:/";
     }
 
-    @GetMapping("/usr/article/list")
+    @GetMapping("/list")
     public String memberList(Model model){
         List articles = articleService.findAll();
         model.addAttribute("articles", articles);
