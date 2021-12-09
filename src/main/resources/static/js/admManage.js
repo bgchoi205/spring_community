fc19ebb6fe41f7c5fae723ae0c597db373666f59
bgchoi205 <choibk4938@gmail.com>
2021-12-09 22:43:36 +0900

    게시판 관리 게시판추가 팝업 추가

---
+++ src/main/resources/static/js/admManage.js
@@ -0,0 +1,9 @@
+
+/* 게시판 관리 - 게시판 추가버튼 */
+$('#addBoardBtn').click(function(){
+  $('#addBoardInput').css('display', 'block');
+});
+
+$('#addBoardClose').click(function(){
+  $('#addBoardInput').css('display', 'none');
+});
